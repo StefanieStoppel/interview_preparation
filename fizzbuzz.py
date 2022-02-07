81eb6ffc1c093133b1eac245e4dc0966f037b68b
Stefanie Stoppel <s.stoppel@gmx.de>
2022-02-07 22:08:31 +0100

    feat: add fizzbuzz task

---
+++ fizzbuzz.py
@@ -0,0 +1,31 @@
+
+# --- FizzBuzz ---
+# Ziel dieser Aufgabe ist es, den folgenden Algorithmus zu implementieren:
+#
+# Fuer alle Zahlen von 1 bis n:
+# - Wenn die Zahl durch 3 teilbar ist, soll der String "Fizz" ausgegeben werden.
+# - Wenn die Zahl durch 5 teilbar ist, soll der String "Buzz" ausgegeben werden.
+# - Wenn die Zahl durch 3 und 5 teilbar ist, soll der String "FizzBuzz" ausgegeben werden.
+# - Wenn keines davon zutrifft, soll einfach nur die Zahl selbst ausgegeben werden.
+# - Das Ergebnis der Aufgabe soll eine Python Liste sein, die alle Ausgaben von 0 bis n enthaelt.
+
+
+def fizzbuzz(n):
+    pass
+
+
+# Unit Tests
+
+def test_three():
+    assert fizzbuzz(3) == [1, 2, "Fizz"]
+
+def test_seven():
+    assert fizzbuzz(7) == [1, 2, "Fizz", 4, "Buzz", "Fizz", 7]
+
+def test_twenty():
+    assert fizzbuzz(20) == [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz", 16, 17, "Fizz", 19, "Buzz"]
+
+if __name__ == '__main__':
+    test_three()
+    test_seven()
+    test_twenty()
